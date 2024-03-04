4253d1234f2e6d5a657e40cf9de85f089efdd6c8
margotriou <margot.riou56@gmail.com>
2024-03-04 11:20:34 +0100

    Moitié des fichiers

--- define.h
+++ define.h
@@ -1,8 +1,47 @@
-//
-// Created by alexm on 04/03/2024.
-//
+/**
+ * @authors Cavaro Alexandre Riou Margot Caen
+ */
 
-#ifndef AUDISEN_DEFINE_H
-#define AUDISEN_DEFINE_H
+#ifndef INCLUDE_H
+#define INCLUDE_H
 
-#endif //AUDISEN_DEFINE_H
+//#define _WIN32
+
+#ifndef _WIN32
+// LINUX INCLUDES
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#endif
+
+#ifdef _WIN32
+// WINDOWS INCLUDES
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#endif
+
+#define MAX_SIZE_TITLE 40
+#define MAX_SIZE_PLAYLIST_NAME 100
+#define MAX_SIZE_LINE 190
+#define MAX_NUMBER_TICKS 999
+#define MAX_SONGNAME_SIZE 100
+
+typedef
+struct tick{
+    int accent;//accentuation du tick (0=Non, 1:Oui)
+    int note[4];// Tableau de 4 notes (0 à 60)
+}s_tick;
+
+typedef
+struct song{
+    int tpm;// tick par minutes
+    int nTicks;// Nombre de ticks dans le morceau
+    char title[MAX_SIZE_TITLE];// Titre du morceau
+    struct tick tickTab[MAX_NUMBER_TICKS];// Tableau de ticks
+}s_song;
+#endif
+
+
+
+
--- frame.h
+++ frame.h
@@ -1,8 +1,26 @@
-//
-// Created by alexm on 04/03/2024.
-//
+/**
+ * @authors Cavaro Alexandre Riou Margot Caen
+ */
 
 #ifndef AUDISEN_FRAME_H
 #define AUDISEN_FRAME_H
 
-#endif //AUDISEN_FRAME_H
+#include "define.h"
+
+#define TICK_FRAME_SIZE 22
+#define INIT_FRAME_MAX_SIZE (1+MAX_SIZE_TITLE+12)
+
+#define MODE 1
+#define ACC 3
+#define N1 5
+#define N2 8
+#define N3 11
+#define N4 14
+
+#define CR 13
+#define LF 10
+
+void createInitFrame(s_song mySong, char* frame);
+void createTickFrame(s_tick myTick, char* frame);
+
+#endif //AUDISEN2024_FRAME_H
